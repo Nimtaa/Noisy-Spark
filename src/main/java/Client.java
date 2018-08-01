f16742a8915b9c130503f77ba6ff88bdcfd0c393
Nimtaa <nima.tavassoli2@gmail.com>
2018-08-01 12:57:51 +0430

    simple client added

---
+++ src/main/java/Client.java
@@ -0,0 +1,43 @@
+
+
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Random;
+
+public class Client {
+
+    private Socket socket = null;
+    private DataOutputStream out  = null;
+
+    public Client (String address,int port){
+
+        try {
+
+            socket = new Socket(address,port);
+            System.out.println("connected");
+            out = new DataOutputStream(socket.getOutputStream());
+
+            while(true) {
+                //out.writeInt((randomNumberGenerator()));
+                out.writeInt(randomNumberGenerator());
+                System.out.println("wrote to output");
+                socket.setSoTimeout(1000);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void main(String[] args) {
+       Client s = new Client("127.0.0.1",9999);
+    }
+
+    static int randomNumberGenerator(){
+        Random rand = new Random();
+        return rand.nextInt(9999)+1000;
+    }
+
+}
