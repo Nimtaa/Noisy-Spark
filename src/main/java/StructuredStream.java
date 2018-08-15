a4de5eb306029e607b7fcb33ba03d95520141b5c
Nimtaa <nima.tavassoli2@gmail.com>
2018-08-15 15:23:17 +0430

    cast timestamp updated

--- src/main/java/StructuredStream.java
+++ src/main/java/StructuredStream.java
@@ -4,7 +4,6 @@ import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.StreamingQueryException;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.sql.types.TimestampType;
-
 import java.util.Arrays;
 
 import static org.apache.spark.sql.functions.*;
@@ -31,29 +30,37 @@ public class StructuredStream {
                 .as(Encoders.STRING())
                 .flatMap((FlatMapFunction<String,String>) x -> Arrays.asList(x.split("\n")).iterator(), Encoders.STRING());
 
+
         Dataset<Row> splitted = europeTemp
-                .withColumn("timestamp",split(col("value"),",").getItem(0))
+//                .withColumn("timestamp",
+//                     unix_timestamp(split(col("value"),",").getItem(0),"yyyy-MM-dd HH:mm:ss.SSS")
+//                .cast("Timestamp"))
+                .withColumn("timestamp",
+                        split(col("value"),",").getItem(0)
+                                .cast("Timestamp"))
                 .withColumn("city",split(col("value"),",").getItem(1))
                 .withColumn("temperature",split(col("value"),",").getItem(2));
 
-
-        Dataset<Row> tostampsplitted = splitted.select
-                (unix_timestamp(splitted.col("timestamp")).cast(TimestampType).as("timestamp"),"city","temperature");
+//        Dataset<Row> timestampsplitted = splitted.select
+//                (unix_timestamp(splitted.col("timestamp")).cast(TimestampType).as("timestamp"),"city","temperature");
 
 
         Dataset<Row> counting = splitted.groupBy("timestamp").count();
         Dataset<Row> withoutValue = splitted.drop(col("value"));
-
-        Dataset<Row> windowedCounts = splitted
-                .withWatermark("timestamp", "1 minutes")
-                .groupBy(
-                        functions.window(splitted.col("timestamp"), "1 minutes", "1 minutes"),
-                        splitted.col("timestamp"))
+//
+//        Dataset<Row> windowedCounts = withoutValue
+//                .withWatermark("timestamp", "1 minutes")
+//                .groupBy(
+//                        functions.window(withoutValue.col("timestamp"), "1 minutes", "30 seconds"),
+//                        withoutValue.col("timestamp"))
+//                .count();
+        Dataset<Row> result = withoutValue.withWatermark("timestamp","1 minutes")
+                .groupBy(functions.window(withoutValue.col("timestamp"),"1 minutes"))
                 .count();
 
-        Dataset<Row> queryResult = withoutValue.select("*").where("temperature > 35");
+        //Dataset<Row> queryResult = withoutValue.select("*").where("temperature > 35");
 
-        StreamingQuery query = withoutValue.writeStream()
+        StreamingQuery query = result.writeStream()
                 .outputMode("append")
                 .format("csv")
                 .option("path","/home/nima/Desktop/temp")
@@ -91,4 +98,17 @@ public class StructuredStream {
 //            e.printStackTrace();
 //        }
     }
+
+//    static Column to_timestamp(Column col){
+//
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+//        Date parsedDate = null;
+//        try {
+//            parsedDate = dateFormat.parse(col.toString());
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//        Timestamp timestamp = new java.sql.Timestamp(parsedDate.getTime());
+//
+//    }
 }
