262876f22946d3a01d7c17d8616c7a835207cf46
Nimtaa <nima.tavassoli2@gmail.com>
2018-08-19 16:08:16 +0430

    stream parquet read fixed

--- src/main/java/ParquetRead.java
+++ src/main/java/ParquetRead.java
@@ -1,9 +1,13 @@
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
 import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.StreamingQueryException;
 import org.apache.spark.sql.types.StructType;
+
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.window;
 
 
 public class ParquetRead {
@@ -11,29 +15,32 @@ public class ParquetRead {
 
         String parquetPath = "/home/nima/Desktop/tempDir/parquet";
         StructType userSchema = new StructType()
-                .add("timestamp","timestamp")
+                .add("timestamp","Timestamp")
                 .add("city","string")
                 .add("temperature","integer");
         SparkSession spark = SparkSession.builder()
                 .appName("parquetread")
                 .getOrCreate();
         spark.sparkContext().setLogLevel("ERROR");
-
         Dataset<Row> ps  = spark
                 .readStream()
                 .schema(userSchema)
                 .parquet(parquetPath);
-        
 
-        StreamingQuery query = ps.writeStream()
-                .outputMode("update")
-                .format("console")
+        Dataset<Row> windowedCount = ps.withWatermark("timestamp", "5 seconds")
+                .groupBy(
+                        functions.window(col("timestamp"), "1 minutes", "30 seconds"),
+                        col("timestamp"))
+                .count();
+
+        StreamingQuery query = windowedCount.writeStream().format("console").outputMode("update")
                 .start();
         try {
             query.awaitTermination();
         } catch (StreamingQueryException e) {
             e.printStackTrace();
         }
+
     }
 
 }
