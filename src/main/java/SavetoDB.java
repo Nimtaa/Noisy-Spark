fc65bf318e7d3b83d707c54f7d0155542c2c4eb3
Nimtaa <nima.tavassoli2@gmail.com>
2018-08-28 13:28:10 +0430

    savetodb class updated

--- src/main/java/SavetoDB.java
+++ src/main/java/SavetoDB.java
@@ -1,73 +1,20 @@
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
 import java.sql.*;
 import java.util.Arrays;
 import static org.apache.spark.sql.functions.col;
 import static org.apache.spark.sql.functions.split;
 
+public class SavetoDB extends ForeachWriter<Row> {
+    PreparedStatement pstmt = null ;
+    Connection conn = null ;
+    SparkSession spark = null;
 
-public class SavetoDB {
-
-    public static void main(String[] args) throws SQLException {
-
-        SparkSession spark = SparkSession.builder().appName("savetodb").getOrCreate();
+    public SavetoDB(){
+        spark = SparkSession.builder().appName("savetodb").getOrCreate();
         spark.sparkContext().setLogLevel("ERROR");
-
-        PreparedStatement pstmt = null;
-        Connection conn = null;
-        Dataset<Row> europeTemp = spark.readStream().format("socket")
-                .option("host","127.0.0.1")
-                .option("port",9998)
-                .load();
-
-        europeTemp
-                .as(Encoders.STRING())
-                .flatMap((FlatMapFunction<String,String>) x -> Arrays.asList(x.split("\n")).iterator(), Encoders.STRING());
-
-
-        Dataset<Row> splitted = europeTemp
-                .withColumn("timestamp",
-                        split(col("value"),",").getItem(0)
-                                .cast("Timestamp"))
-                .withColumn("city",split(col("value"),",").getItem(1))
-                .withColumn("temperature",split(col("value"),",").getItem(2));
-
-        
-       System.out.println("City column :" + col("city"));
-
-//
-//        StreamingQuery query = splitted.writeStream()
-//                .outputMode("update")
-//                .format("console")
-//                .start();
-
-        //Direct Way using write from spark
-
-
-        //Indirect way using database connection and preparestatement
-        conn = getConnection();
-        try {
-            conn.setAutoCommit(false);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        try {
-            pstmt = conn.prepareStatement("insert into citytemp(date, city ,temperature) values (?, ?, ?);");
-            pstmt.setTimestamp(1, Timestamp.valueOf( col("timestamp").toString()));
-            pstmt.setString(2, col("city").toString());
-            pstmt.setInt(3, 24);
-            pstmt.executeUpdate();
-            conn.commit();
-            System.out.println("query executed");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
     }
+
     public static Connection getConnection()  {
         String driver = "com.mysql.cj.jdbc.Driver";
         String url = "jdbc:mysql://localhost/testdb";
@@ -89,4 +36,65 @@ public class SavetoDB {
         }
         return conn;
     }
+    @Override
+    public boolean open(long l, long l1) {
+        conn = getConnection();
+        try {
+            conn.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+    @Override
+    public void process(Row row) {
+        try {
+            pstmt = conn.prepareStatement("select into citytemp values (?,?,?)");
+            //pstmt.setTimestamp(1, Timestamp.valueOf( col("timestamp").toString()));
+            pstmt.setString(1,row.get(0).toString());
+            pstmt.setString(2, row.get(1).toString());
+            pstmt.setInt(3, 24);
+            pstmt.executeUpdate();
+            System.out.println("query executed");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void close(Throwable throwable) {
+        try {
+            conn.commit();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args)  {
+
+        ForeachWriter<Row> writer = new SavetoDB();
+
+        Dataset<Row> europeTemp = ((SavetoDB) writer).spark.readStream().format("socket")
+                .option("host","127.0.0.1")
+                .option("port",9998)
+                .load();
+
+        europeTemp
+                .as(Encoders.STRING())
+                .flatMap((FlatMapFunction<String,String>) x -> Arrays.asList(x.split("\n")).iterator(), Encoders.STRING());
+
+
+        Dataset<Row> splitted = europeTemp
+                .withColumn("timestamp",
+                        split(col("value"),",").getItem(0)
+                                .cast("Timestamp"))
+                .withColumn("city",split(col("value"),",").getItem(1))
+                .withColumn("temperature",split(col("value"),",").getItem(2));
+
+        splitted.writeStream().foreach(writer).start();
+
+
+    }
+
 }
